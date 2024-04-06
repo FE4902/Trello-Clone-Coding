c05f363dfbd1327d0ac414525c9d88f8862a4c46
FE4902 <isdn000123@gmail.com>
2024-04-06 16:30:33 +0900

    Same Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -24,19 +24,20 @@ const Boards = styled.div`
 function App() {
     const [toDos, setToDos] = useRecoilState(toDoState);
 
-    const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
-        if (!destination) return;
+    const onDragEnd = (info: DropResult) => {
+        const { destination, draggableId, source } = info;
 
-        // setToDos((oldToDos) => {
-        //     const toDosCopy = [...oldToDos];
-
-        //     // 1) Delete item on source.index
-        //     toDosCopy.splice(source.index, 1);
-        //     // 2) Put back the item on the destination.index
-        //     toDosCopy.splice(destination?.index, 0, draggableId);
-
-        //     return toDosCopy;
-        // });
+        if (destination?.droppableId === source.droppableId) {
+            setToDos((allBoards) => {
+                const boardCopy = [...allBoards[source.droppableId]];
+                boardCopy.splice(source.index, 1);
+                boardCopy.splice(destination?.index, 0, draggableId);
+                return {
+                    ...allBoards,
+                    [source.droppableId]: boardCopy,
+                };
+            });
+        }
     };
 
     return (
--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -3,10 +3,17 @@ import DragabbleCard from "./DragabbleCard";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-    min-height: 200px;
-    padding: 30px 20px 20px;
+    min-height: 300px;
+    padding: 20px 10px 10px;
     border-radius: 5px;
     background-color: ${(props) => props.theme.boardColor};
+`;
+
+const Title = styled.h2`
+    margin-bottom: 10px;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 600;
 `;
 
 interface IBoardProps {
@@ -16,15 +23,22 @@ interface IBoardProps {
 
 export default function Board({ toDos, boardId }: IBoardProps) {
     return (
-        <Droppable droppableId={boardId}>
-            {(magic) => (
-                <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
-                    {toDos.map((toDo, index) => (
-                        <DragabbleCard key={toDo} index={index} toDo={toDo} />
-                    ))}
-                    {magic.placeholder}
-                </Wrapper>
-            )}
-        </Droppable>
+        <Wrapper>
+            <Title>{boardId}</Title>
+            <Droppable droppableId={boardId}>
+                {(magic) => (
+                    <div ref={magic.innerRef} {...magic.droppableProps}>
+                        {toDos.map((toDo, index) => (
+                            <DragabbleCard
+                                key={toDo}
+                                index={index}
+                                toDo={toDo}
+                            />
+                        ))}
+                        {magic.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </Wrapper>
     );
 }
